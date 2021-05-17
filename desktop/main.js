8af48f545a4e192126ab98803f7c5980f6380bb2
cfp <chaosflo44plus@gmail.com>
2021-05-17 13:17:15 +0200

    Commit (Still broken)

--- desktop/main.js
+++ desktop/main.js
@@ -5,8 +5,8 @@ process.env.NODE_ENV = "development";
 app.whenReady().then(() => {
 
     const window = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 1000,
         autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: false
@@ -17,36 +17,5 @@ app.whenReady().then(() => {
         app.quit();
     });
 
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-
     window.loadFile("page/app/app.html"); // neeto find a better way to do this instead of needing to copy this into the page folder ../ wont work
 });
-
-// Catch item:add
-ipcMain.on('item:add', function(e, item){
-    mainWindow.webContents.send('item:add', item);
-    addWindow.close(); 
-    // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
-    //addWindow = null;
-});
-
-const mainMenuTemplate =  [];
-
-// Add developer tools option if in dev
-if(process.env.NODE_ENV !== 'production'){
-    mainMenuTemplate.push({
-      label: 'Developer Tools',
-      submenu:[
-        {
-          role: 'reload'
-        },
-        {
-          label: 'Toggle DevTools',
-          accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-          click(item, focusedWindow){
-            focusedWindow.toggleDevTools();
-          }
-        }
-      ]
-    });
-  }
--- desktop/platform/app/platform.js
+++
@@ -1,5 +0,0 @@
-
-
-window.addEventListener("DOMContentLoaded", () => {
-    
-});
